feef81b28eb1205cac076ec8865778343dedfa14
pc <pc@LENNY>
2024-03-08 10:38:22 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio5_hospital/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Ejercicio5_hospital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_hospital = new System.Windows.Forms.Label();
+            this.lblResultadoEmergencia = new System.Windows.Forms.Label();
+            this.lblResultadoSuministros = new System.Windows.Forms.Label();
+            this.lbl_resultadoCirugia = new System.Windows.Forms.Label();
+            this.lbl_presupuesto2 = new System.Windows.Forms.Label();
+            this.lblResultadoImagenes = new System.Windows.Forms.Label();
+            this.lbl_imagenes = new System.Windows.Forms.Label();
+            this.lbl_emergencia = new System.Windows.Forms.Label();
+            this.lbl_asignacion = new System.Windows.Forms.Label();
+            this.lbl_presupuesto = new System.Windows.Forms.Label();
+            this.lbl_suministros = new System.Windows.Forms.Label();
+            this.lbl_administracion = new System.Windows.Forms.Label();
+            this.lblResultadoLaboratorio = new System.Windows.Forms.Label();
+            this.lblResultadoAdministracion = new System.Windows.Forms.Label();
+            this.lbl_laboratorio = new System.Windows.Forms.Label();
+            this.lbl_cirugia = new System.Windows.Forms.Label();
+            this.lbl_departamento = new System.Windows.Forms.Label();
+            this.btn_repartir = new System.Windows.Forms.Button();
+            this.lbl_limpiar = new System.Windows.Forms.Button();
+            this.lbl_cerrar = new System.Windows.Forms.Button();
+            this.txt_presupuesto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_hospital
+            // 
+            this.lbl_hospital.AutoSize = true;
+            this.lbl_hospital.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_hospital.Location = new System.Drawing.Point(106, 64);
+            this.lbl_hospital.Name = "lbl_hospital";
+            this.lbl_hospital.Size = new System.Drawing.Size(160, 16);
+            this.lbl_hospital.TabIndex = 0;
+            this.lbl_hospital.Text = "Presupuesto del hospital";
+            // 
+            // lblResultadoEmergencia
+            // 
+            this.lblResultadoEmergencia.AutoSize = true;
+            this.lblResultadoEmergencia.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoEmergencia.Location = new System.Drawing.Point(312, 169);
+            this.lblResultadoEmergencia.Name = "lblResultadoEmergencia";
+            this.lblResultadoEmergencia.Size = new System.Drawing.Size(87, 16);
+            this.lblResultadoEmergencia.TabIndex = 1;
+            this.lblResultadoEmergencia.Text = "Esperando....";
+            // 
+            // lblResultadoSuministros
+            // 
+            this.lblResultadoSuministros.AutoSize = true;
+            this.lblResultadoSuministros.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoSuministros.Location = new System.Drawing.Point(312, 218);
+            this.lblResultadoSuministros.Name = "lblResultadoSuministros";
+            this.lblResultadoSuministros.Size = new System.Drawing.Size(87, 16);
+            this.lblResultadoSuministros.TabIndex = 2;
+            this.lblResultadoSuministros.Text = "Esperando....";
+            // 
+            // lbl_resultadoCirugia
+            // 
+            this.lbl_resultadoCirugia.AutoSize = true;
+            this.lbl_resultadoCirugia.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_resultadoCirugia.Location = new System.Drawing.Point(312, 196);
+            this.lbl_resultadoCirugia.Name = "lbl_resultadoCirugia";
+            this.lbl_resultadoCirugia.Size = new System.Drawing.Size(87, 16);
+            this.lbl_resultadoCirugia.TabIndex = 3;
+            this.lbl_resultadoCirugia.Text = "Esperando....";
+            // 
+            // lbl_presupuesto2
+            // 
+            this.lbl_presupuesto2.AutoSize = true;
+            this.lbl_presupuesto2.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_presupuesto2.Location = new System.Drawing.Point(413, 93);
+            this.lbl_presupuesto2.Name = "lbl_presupuesto2";
+            this.lbl_presupuesto2.Size = new System.Drawing.Size(83, 16);
+            this.lbl_presupuesto2.TabIndex = 4;
+            this.lbl_presupuesto2.Text = "Esperando...";
+            // 
+            // lblResultadoImagenes
+            // 
+            this.lblResultadoImagenes.AutoSize = true;
+            this.lblResultadoImagenes.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoImagenes.Location = new System.Drawing.Point(312, 295);
+            this.lblResultadoImagenes.Name = "lblResultadoImagenes";
+            this.lblResultadoImagenes.Size = new System.Drawing.Size(87, 16);
+            this.lblResultadoImagenes.TabIndex = 5;
+            this.lblResultadoImagenes.Text = "Esperando....";
+            // 
+            // lbl_imagenes
+            // 
+            this.lbl_imagenes.AutoSize = true;
+            this.lbl_imagenes.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_imagenes.Location = new System.Drawing.Point(154, 295);
+            this.lbl_imagenes.Name = "lbl_imagenes";
+            this.lbl_imagenes.Size = new System.Drawing.Size(149, 16);
+            this.lbl_imagenes.TabIndex = 6;
+            this.lbl_imagenes.Text = "Imagenes Diagnosticas";
+            // 
+            // lbl_emergencia
+            // 
+            this.lbl_emergencia.AutoSize = true;
+            this.lbl_emergencia.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_emergencia.Location = new System.Drawing.Point(154, 169);
+            this.lbl_emergencia.Name = "lbl_emergencia";
+            this.lbl_emergencia.Size = new System.Drawing.Size(78, 16);
+            this.lbl_emergencia.TabIndex = 7;
+            this.lbl_emergencia.Text = "Emergencia";
+            // 
+            // lbl_asignacion
+            // 
+            this.lbl_asignacion.AutoSize = true;
+            this.lbl_asignacion.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_asignacion.Location = new System.Drawing.Point(312, 123);
+            this.lbl_asignacion.Name = "lbl_asignacion";
+            this.lbl_asignacion.Size = new System.Drawing.Size(80, 18);
+            this.lbl_asignacion.TabIndex = 8;
+            this.lbl_asignacion.Text = "Asignacion";
+            // 
+            // lbl_presupuesto
+            // 
+            this.lbl_presupuesto.AutoSize = true;
+            this.lbl_presupuesto.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_presupuesto.Location = new System.Drawing.Point(413, 64);
+            this.lbl_presupuesto.Name = "lbl_presupuesto";
+            this.lbl_presupuesto.Size = new System.Drawing.Size(83, 16);
+            this.lbl_presupuesto.TabIndex = 9;
+            this.lbl_presupuesto.Text = "Presupuesto";
+            // 
+            // lbl_suministros
+            // 
+            this.lbl_suministros.AutoSize = true;
+            this.lbl_suministros.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_suministros.Location = new System.Drawing.Point(154, 218);
+            this.lbl_suministros.Name = "lbl_suministros";
+            this.lbl_suministros.Size = new System.Drawing.Size(80, 16);
+            this.lbl_suministros.TabIndex = 10;
+            this.lbl_suministros.Text = "Suministros";
+            // 
+            // lbl_administracion
+            // 
+            this.lbl_administracion.AutoSize = true;
+            this.lbl_administracion.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_administracion.Location = new System.Drawing.Point(154, 242);
+            this.lbl_administracion.Name = "lbl_administracion";
+            this.lbl_administracion.Size = new System.Drawing.Size(101, 16);
+            this.lbl_administracion.TabIndex = 11;
+            this.lbl_administracion.Text = "Administracion";
+            // 
+            // lblResultadoLaboratorio
+            // 
+            this.lblResultadoLaboratorio.AutoSize = true;
+            this.lblResultadoLaboratorio.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoLaboratorio.Location = new System.Drawing.Point(312, 266);
+            this.lblResultadoLaboratorio.Name = "lblResultadoLaboratorio";
+            this.lblResultadoLaboratorio.Size = new System.Drawing.Size(87, 16);
+            this.lblResultadoLaboratorio.TabIndex = 12;
+            this.lblResultadoLaboratorio.Text = "Esperando....";
+            // 
+            // lblResultadoAdministracion
+            // 
+            this.lblResultadoAdministracion.AutoSize = true;
+            this.lblResultadoAdministracion.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoAdministracion.Location = new System.Drawing.Point(312, 242);
+            this.lblResultadoAdministracion.Name = "lblResultadoAdministracion";
+            this.lblResultadoAdministracion.Size = new System.Drawing.Size(87, 16);
+            this.lblResultadoAdministracion.TabIndex = 13;
+            this.lblResultadoAdministracion.Text = "Esperando....";
+            // 
+            // lbl_laboratorio
+            // 
+            this.lbl_laboratorio.AutoSize = true;
+            this.lbl_laboratorio.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_laboratorio.Location = new System.Drawing.Point(154, 266);
+            this.lbl_laboratorio.Name = "lbl_laboratorio";
+            this.lbl_laboratorio.Size = new System.Drawing.Size(79, 16);
+            this.lbl_laboratorio.TabIndex = 14;
+            this.lbl_laboratorio.Text = "Laboratorio";
+            // 
+            // lbl_cirugia
+            // 
+            this.lbl_cirugia.AutoSize = true;
+            this.lbl_cirugia.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_cirugia.Location = new System.Drawing.Point(154, 196);
+            this.lbl_cirugia.Name = "lbl_cirugia";
+            this.lbl_cirugia.Size = new System.Drawing.Size(51, 16);
+            this.lbl_cirugia.TabIndex = 15;
+            this.lbl_cirugia.Text = "Cirugia";
+            // 
+            // lbl_departamento
+            // 
+            this.lbl_departamento.AutoSize = true;
+            this.lbl_departamento.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_departamento.Location = new System.Drawing.Point(154, 123);
+            this.lbl_departamento.Name = "lbl_departamento";
+            this.lbl_departamento.Size = new System.Drawing.Size(98, 18);
+            this.lbl_departamento.TabIndex = 16;
+            this.lbl_departamento.Text = "Departamento";
+            // 
+            // btn_repartir
+            // 
+            this.btn_repartir.BackColor = System.Drawing.Color.Lime;
+            this.btn_repartir.Location = new System.Drawing.Point(563, 89);
+            this.btn_repartir.Name = "btn_repartir";
+            this.btn_repartir.Size = new System.Drawing.Size(75, 23);
+            this.btn_repartir.TabIndex = 17;
+            this.btn_repartir.Text = "Repartir";
+            this.btn_repartir.UseVisualStyleBackColor = false;
+            this.btn_repartir.Click += new System.EventHandler(this.btn_repartir_Click);
+            // 
+            // lbl_limpiar
+            // 
+            this.lbl_limpiar.BackColor = System.Drawing.Color.Yellow;
+            this.lbl_limpiar.Location = new System.Drawing.Point(563, 118);
+            this.lbl_limpiar.Name = "lbl_limpiar";
+            this.lbl_limpiar.Size = new System.Drawing.Size(75, 23);
+            this.lbl_limpiar.TabIndex = 18;
+            this.lbl_limpiar.Text = "Limpiar";
+            this.lbl_limpiar.UseVisualStyleBackColor = false;
+            this.lbl_limpiar.Click += new System.EventHandler(this.lbl_limpiar_Click);
+            // 
+            // lbl_cerrar
+            // 
+            this.lbl_cerrar.BackColor = System.Drawing.Color.Red;
+            this.lbl_cerrar.Location = new System.Drawing.Point(563, 147);
+            this.lbl_cerrar.Name = "lbl_cerrar";
+            this.lbl_cerrar.Size = new System.Drawing.Size(75, 23);
+            this.lbl_cerrar.TabIndex = 19;
+            this.lbl_cerrar.Text = "Cerrar";
+            this.lbl_cerrar.UseVisualStyleBackColor = false;
+            this.lbl_cerrar.Click += new System.EventHandler(this.lbl_cerrar_Click);
+            // 
+            // txt_presupuesto
+            // 
+            this.txt_presupuesto.Location = new System.Drawing.Point(272, 62);
+            this.txt_presupuesto.Name = "txt_presupuesto";
+            this.txt_presupuesto.Size = new System.Drawing.Size(100, 20);
+            this.txt_presupuesto.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(693, 345);
+            this.Controls.Add(this.txt_presupuesto);
+            this.Controls.Add(this.lbl_cerrar);
+            this.Controls.Add(this.lbl_limpiar);
+            this.Controls.Add(this.btn_repartir);
+            this.Controls.Add(this.lbl_departamento);
+            this.Controls.Add(this.lbl_cirugia);
+            this.Controls.Add(this.lbl_laboratorio);
+            this.Controls.Add(this.lblResultadoAdministracion);
+            this.Controls.Add(this.lblResultadoLaboratorio);
+            this.Controls.Add(this.lbl_administracion);
+            this.Controls.Add(this.lbl_suministros);
+            this.Controls.Add(this.lbl_presupuesto);
+            this.Controls.Add(this.lbl_asignacion);
+            this.Controls.Add(this.lbl_emergencia);
+            this.Controls.Add(this.lbl_imagenes);
+            this.Controls.Add(this.lblResultadoImagenes);
+            this.Controls.Add(this.lbl_presupuesto2);
+            this.Controls.Add(this.lbl_resultadoCirugia);
+            this.Controls.Add(this.lblResultadoSuministros);
+            this.Controls.Add(this.lblResultadoEmergencia);
+            this.Controls.Add(this.lbl_hospital);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_hospital;
+        private System.Windows.Forms.Label lblResultadoEmergencia;
+        private System.Windows.Forms.Label lblResultadoSuministros;
+        private System.Windows.Forms.Label lbl_resultadoCirugia;
+        private System.Windows.Forms.Label lbl_presupuesto2;
+        private System.Windows.Forms.Label lblResultadoImagenes;
+        private System.Windows.Forms.Label lbl_imagenes;
+        private System.Windows.Forms.Label lbl_emergencia;
+        private System.Windows.Forms.Label lbl_asignacion;
+        private System.Windows.Forms.Label lbl_presupuesto;
+        private System.Windows.Forms.Label lbl_suministros;
+        private System.Windows.Forms.Label lbl_administracion;
+        private System.Windows.Forms.Label lblResultadoLaboratorio;
+        private System.Windows.Forms.Label lblResultadoAdministracion;
+        private System.Windows.Forms.Label lbl_laboratorio;
+        private System.Windows.Forms.Label lbl_cirugia;
+        private System.Windows.Forms.Label lbl_departamento;
+        private System.Windows.Forms.Button btn_repartir;
+        private System.Windows.Forms.Button lbl_limpiar;
+        private System.Windows.Forms.Button lbl_cerrar;
+        private System.Windows.Forms.TextBox txt_presupuesto;
+    }
+}
+
---
+++ Ejercicio5_hospital/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio5_hospital
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+            lblResultadoAdministracion.Visible = false;
+            lblResultadoEmergencia.Visible = false;
+            lblResultadoImagenes.Visible = false;
+            lblResultadoLaboratorio.Visible = false;
+            lblResultadoSuministros.Visible = false;
+            lbl_presupuesto2.Visible = false;
+            lbl_resultadoCirugia.Visible = false;
+            lbl_asignacion.Visible = false;
+        }
+
+        private void btn_repartir_Click(object sender, EventArgs e)
+        {
+            double Presupuesto, Emergencia, Cirugia, Suministros, Administracion, Laboratorio, ImagenesDiagnosticas;
+
+            Presupuesto = int.Parse(txt_presupuesto.Text);
+
+            lblResultadoAdministracion.Visible = true;
+            lblResultadoEmergencia.Visible = true;
+            lblResultadoImagenes.Visible = true;
+            lblResultadoLaboratorio.Visible = true;
+            lblResultadoSuministros.Visible = true;
+            lbl_presupuesto2.Visible = true;
+            lbl_resultadoCirugia.Visible = true;
+            lbl_asignacion.Visible = true;
+
+
+            Emergencia = Presupuesto * 0.30;
+            Cirugia = Presupuesto * 0.15;
+            Suministros = Presupuesto * 0.25;
+            Administracion = Presupuesto * 0.18;
+            Laboratorio = Presupuesto * 0.05;
+            ImagenesDiagnosticas = Presupuesto * 0.07;
+
+
+            lbl_presupuesto2.Text = Presupuesto.ToString();
+            lblResultadoEmergencia.Text = Emergencia.ToString();
+            lbl_resultadoCirugia.Text = Cirugia.ToString();
+            lblResultadoSuministros.Text = Suministros.ToString();
+            lblResultadoAdministracion.Text = Administracion.ToString();
+            lblResultadoLaboratorio.Text = Laboratorio.ToString();
+            lblResultadoImagenes.Text = ImagenesDiagnosticas.ToString();
+        }
+
+        private void lbl_cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lbl_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_presupuesto.Clear();
+            txt_presupuesto.Focus();
+            lblResultadoEmergencia.Text = String.Empty;
+            lbl_resultadoCirugia.Text = String.Empty;
+            lblResultadoImagenes.Text = String.Empty;
+            lblResultadoAdministracion.Text = String.Empty;
+            lbl_presupuesto2.Text = String.Empty;
+            lblResultadoLaboratorio.Text = String.Empty;
+            lblResultadoSuministros.Text = String.Empty;
+            lbl_asignacion.Visible = false;
+        }
+    }
+}
